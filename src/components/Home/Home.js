b98ce6512b116dedc9b1e59a208dde0992c3e731
Aaron Clark <amclarkwustl@gmail.com>
2023-08-30 22:40:21 -0700

    set up letter animations on home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+// import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import './Home.scss'
+import LogoTitle from '../../assets/images/logo-a2.png';
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['a', 'r', 'o', 'n']
+  const jobArray = ['s','o','f','t','w','a','r','e',' ','d','e','v','e','l','o','p','e','r','.']
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <div className = "container home-page">
+      <div className="text-zone">
+        <h1> 
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+        <img src={LogoTitle} alt="Capital A" />
+        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+        <br />
+        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={12} />
+        </h1>
+        <h2>Frontend Developer / Javascript and Java specialist</h2>
+        <Link to="/contact" className = 'flat-button'>CONTACT ME</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -10,13 +10,13 @@ const Sidebar = () => (
       <FontAwesomeIcon className='logo-icon' icon={faMicrochip} size='2x' aria-label='Microchip Icon' />
     </Link>
     <nav>
-      <NavLink exact="true" activeClassname="active" to="/">
+      <NavLink exact="true" activeclassname="active" to="/">
         <FontAwesomeIcon icon={faHome} color ="4d4d4e" />
       </NavLink>
-      <NavLink exact="true" activeClassname="active" className="about-link" to="/about">
+      <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
         <FontAwesomeIcon icon={faUser} color ="4d4d4e" />
       </NavLink>
-      <NavLink exact="true" activeClassname="active" className="contact-link" to="/contact">
+      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
         <FontAwesomeIcon icon={faEnvelope} color ="4d4d4e" />
       </NavLink>
     </nav>
