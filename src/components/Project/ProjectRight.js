a1320c725bb7a15566561b690484ba70aaacefae
Aaron Clark <amclarkwustl@gmail.com>
2023-09-26 20:15:34 -0700

    added content for third project and fixed styling of projects component

--- src/components/Project/ProjectRight.js
+++ src/components/Project/ProjectRight.js
@@ -2,50 +2,64 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import tarotImage from '../../assets/images/TarotScreens.jpg';
+import fridgeImage from '../../assets/images/FridgeMates.jpg';
+import cineWatch from '../../assets/images/cinewatch.jpg';
 import './ProjectRight.scss';
 
 const ProjectRight = () => {
   return (
     <>
-      <h1 className="section-heading">Projects</h1>
+      <h1 className="section-heading">Some of My Projects</h1>
       <div className="project-container">
         <div className="project">
           <div className="project-content">
-            <div className="project-label">Game of Thrones</div>
-            <h4 className="project-title">A Clash of Kings</h4>
+          <div className="project-label">Android Application</div>
+            <h4 className="project-title">Daily Tarot App</h4>
             <div className="project-details">
-              <p>Lorem ipsum dolor amet you probably haven't heard of them bitters selvage listicle heirloom. Locavore kombucha street art ennui 90's, organic food truck hell of seitan skateboard literally hexagon fixie next level.</p>
+              <p>Android Tarot reading application that allows users to generate a reading and view card explanations to learn more about Tarot. Coming soon to Google Play Store.</p>
               <ul className="project-tech-list">
-                <li>Shadow</li>
-                <li>Demon</li>
-                <li>Baby</li>
+                <li>Java</li>
+                <li>AWS Amplify</li>
+                <li>SQLite</li>
               </ul>
               <div className="project-links">
-                <a target="_blank" aria-label="External Link" rel="noreferrer" href="https://github.com/amcwustl">
+                <a
+                  target="_blank"
+                  aria-label="External Link"
+                  rel="noreferrer"
+                  href="https://github.com/amcwustl"
+                  title="Coming Soon to Google Play"
+                >
                   <FontAwesomeIcon icon={faExternalLinkSquareAlt} color="4d4d4e" />
                 </a>
-                <a target="_blank" aria-label="Github Repo" rel="noreferrer" href="https://github.com/amcwustl">
+                <a
+                  target="_blank"
+                  aria-label="Github Repo"
+                  rel="noreferrer"
+                  href="https://github.com/JavaWookies/Daily-Tarot"
+                  title="View Github Repo"
+                >
                   <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
                 </a>
               </div>
             </div>
           </div>
-
           <div className="project-img">
-            <img src="https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png" alt="" />
+            <img src={tarotImage} alt="Screenshots of Daily Tarot App" />
           </div>
         </div>
 
         <div className="project">
           <div className="project-content">
-            <div className="project-label">Game of Thrones</div>
-            <h4 className="project-title">A Clash of Kings</h4>
+            <div className="project-label">Spring Boot Application</div>
+            <h4 className="project-title">Fridge Mates</h4>
             <div className="project-details">
-              <p>Lorem ipsum dolor amet you probably haven't heard of them bitters selvage listicle heirloom. Locavore kombucha street art ennui 90's, organic food truck hell of seitan skateboard literally hexagon fixie next level.</p>
+              <p>Refrigerator content tracking social application allowing users to track expiration dates on ingredients, find other users in their area and propose trades and exchanges.</p>
               <ul className="project-tech-list">
-                <li>Shadow</li>
-                <li>Demon</li>
-                <li>Baby</li>
+                <li>Java</li>
+                <li>Spring Boot</li>
+                <li>PostgreSQL</li>
               </ul>
               <div className="project-links">
                 <a target="_blank" aria-label="External Link" rel="noreferrer" href="https://github.com/amcwustl">
@@ -57,11 +71,37 @@ const ProjectRight = () => {
               </div>
             </div>
           </div>
-
           <div className="project-img">
-            <img src="https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png" alt="" />
+            <img src={fridgeImage} alt="Home page of Fridge Mates Application" />
           </div>
         </div>
+
+        <div className="project">
+          <div className="project-content">
+            <div className="project-label">React Application</div>
+            <h4 className="project-title">CinewatchAI</h4>
+            <div className="project-details">
+              <p>OpenAI integrated movie content reviewer that allows users to assess the maturity levels of movie content for children across a number of different categories.</p>
+              <ul className="project-tech-list">
+                <li>React</li>
+                <li>Node.js</li>
+                <li>MongoDB</li>
+              </ul>
+              <div className="project-links">
+                <a target="_blank" aria-label="External Link" rel="noreferrer" href="https://cinewatch-ai.netlify.app/">
+                  <FontAwesomeIcon icon={faExternalLinkSquareAlt} color="4d4d4e" />
+                </a>
+                <a target="_blank" aria-label="Github Repo" rel="noreferrer" href="https://github.com/AIOpenWorkSpace">
+                  <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="project-img">
+            <img src={cineWatch} alt="Home page of Fridge Mates Application" />
+          </div>
+        </div>
+
       </div>
     </>
   )
