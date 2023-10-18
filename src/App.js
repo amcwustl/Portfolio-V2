200bad3fd637d351697f4ac7a67b643ff1c57300
Aaron Clark <amclarkwustl@gmail.com>
2023-10-17 20:46:45 -0700

    updated mobile view fonts to be more opaque.  Made background img of projects more transparent and removed click animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.scss';
 import Home from './components/Home/Home';
 
 function App() {
+  console.log('Design inspired by Brittany Chiang\'s portfolio at https://brittanychiang.com.');
+
   return (
     <div className="App">
       <Home />
